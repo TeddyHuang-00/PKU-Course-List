fadf072f55e0a1ee49284f6eb74a45a49021338f
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-02-23 19:36:27 +0800

    More checks on query parameters

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import functools as ft
 import logging
 import os
 import re
 from argparse import ArgumentParser
 from collections import namedtuple
 from multiprocessing import Pool
-import functools as ft
 
+import bs4
 import pandas as pd
 import requests
 from rich.logging import RichHandler
@@ -21,6 +22,7 @@ headers = {
 data = Query("", "", "22-23-1", "0", "0")
 HTML_tag_pattern = re.compile("<.*?>")
 request_url = "http://www.dean.pku.edu.cn/service/web/courseSearch_do.php"
+base_url = "http://www.dean.pku.edu.cn/service/web/courseSearch.php"
 colnames = [
     "序号",
     "课程号",
@@ -49,7 +51,7 @@ def stripHTMLtags(text):
     return re.sub(HTML_tag_pattern, "", text)
 
 
-def _post(query: Query = data, startrow: str = "0"):
+def _post(query: Query, startrow: str = "0"):
     global logger
     logger.debug(f"POST {request_url} with {query._asdict() | {'startrow': startrow}}")
     return requests.post(
@@ -57,11 +59,7 @@ def _post(query: Query = data, startrow: str = "0"):
     )
 
 
-def getCourseListPart(
-    query: Query = data,
-    startrow: str = "0",
-    retry: int = 3,
-):
+def getCourseListPart(query: Query, startrow: str, retry: int):
     global logger
     response = _post(query, startrow)
     while response.status_code != 200 and retry > 0:
@@ -89,7 +87,7 @@ def getCourseListPart(
     return df
 
 
-def getTotalCount(query: Query = data, retry: int = 3):
+def getTotalCount(query: Query, retry: int):
     global logger
     response = _post(query)
     while response.status_code != 200 and retry > 0:
@@ -104,28 +102,59 @@ def getTotalCount(query: Query = data, retry: int = 3):
         )
         return None
     json = response.json()
-    logger.info(f"Successfully got course count")
+    logger.info(f"Successfully got course count {int(json['count'])}")
     return int(json["count"])
+
+
+def getOptions(retry: int):
+    global logger
+    logger.debug(f"GET {base_url}")
+    response = requests.get(base_url, headers=headers)
+    while response.status_code != 200 and retry > 0:
+        logger.warning(
+            f"Got status code {response.status_code} from server, retrying {retry} times left..."
+        )
+        response = requests.get(base_url, headers=headers)
+        retry -= 1
+    if response.status_code != 200:
+        logger.critical(f"Failed to get options, aborting...")
+        return None
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    yuanxi: dict[str, str] = {
+        item["data"]: item.text
+        for item in soup.find_all("span", {"class": "yuanxi"}, recursive=True)
+    }
+    coursetype: dict[str, str] = {
+        item["data"]: item.text
+        for item in soup.find_all("span", {"class": "coursetype"}, recursive=True)
+    }
+    return yuanxi, coursetype
 
 
 def getCourseList(query: Query = data, retry: int = 3):
     global logger
-    total_count = getTotalCount(query)
-    while not total_count and retry > 0:
+    total_count = getTotalCount(query, retry)
+    while total_count is None and retry > 0:
         logger.warning(
             f"Failed to get course count from server, retrying {retry} times left..."
         )
-        total_count = getTotalCount(query)
+        total_count = getTotalCount(query, retry)
         retry -= 1
-    if not total_count:
-        logger.fatal(f"Failed to get total count, aborting...")
+    if total_count is None:
+        logger.critical(f"Failed to get total count, aborting...")
+        return None
+    if total_count == 0:
+        logger.info(f"Got 0 matching result, aborting...")
         return None
     segs = list(range(0, total_count, 10))
     logger.info(f"Got {total_count} courses, {len(segs)} segments to fetch")
     collect: list[pd.DataFrame | None] = [None] * len(segs)
     pool = Pool()
     while len(segs) and retry > 0:
-        result = pool.map(ft.partial(getCourseListPart, query), map(str, segs))
+        result = pool.map(
+            ft.partial(getCourseListPart, query, retry=retry), map(str, segs)
+        )
         for idx, item in zip(segs, result):
             collect[idx // 10] = item
         segs = [idx * 10 for idx, item in enumerate(collect) if item is None]
@@ -137,6 +166,34 @@ def getCourseList(query: Query = data, retry: int = 3):
     else:
         logger.info(f"Successfully fetched all segments")
     return pd.concat([item for item in collect if item is not None])
+
+
+def isValidQuery(query: Query, retry: int):
+    options = getOptions(retry)
+    if options is None:
+        logger.critical("Failed to get options")
+        return False
+    yuanxi, coursetype = options
+    if query.yuanxi not in yuanxi.keys():
+        logger.critical("Valid yuanxi values and meanings are:")
+        for k, v in yuanxi.items():
+            logger.critical(f"{k}: {v}")
+        logger.critical(f"But got invalid yuanxi code: {query.yuanxi}")
+        return False
+    if query.coursetype not in coursetype.keys():
+        logger.critical("Valid coursetype values and meanings are:")
+        for k, v in coursetype.items():
+            logger.critical(f"{k}: {v}")
+        logger.critical(f"But got invalid coursetype code: {query.coursetype}")
+        return False
+    year_s, year_e, semester = map(int, query.yearandseme.split("-"))
+    if year_e != year_s + 1 or semester < 1 or semester > 3:
+        logger.critical(f"Invalid yearandseme: {query.yearandseme}")
+        logger.critical(
+            f"Did you mean {min(year_s, year_e)}-{min(year_s, year_e) + 1}-{min(max(semester, 1), 3)}?"
+        )
+        return False
+    return True
 
 
 def main():
@@ -167,8 +224,8 @@ def main():
         "-y",
         "--yuanxi",
         help="School/department to look up for (this is the code for school/department, default 0 for all)",
-        type=int,
-        default=0,
+        type=str,
+        default="0",
     )
     argparser.add_argument(
         "-r",
@@ -192,7 +249,7 @@ def main():
     )
     argparser.add_argument(
         "YearAndSeme",
-        help="Year and sememster to look up for (default 22-23-1 stands for the first sememster in year 2022-2023)",
+        help="Year and semester to look up for (default 22-23-1 stands for the first semester in year 2022-2023)",
         type=str,
         default="22-23-1",
     )
@@ -206,6 +263,9 @@ def main():
         str(args.yuanxi),
     )
     logger.info(f"Querying {query2str(query)}")
+    if not isValidQuery(query, args.retry):
+        logger.critical(f"Encountered invalid query parameters, aborting...")
+        return
     if os.path.exists(f"{query2str(query)}.csv") and not args.force:
         logger.info(
             f"File {query2str(query)}.csv already exists, use -f to force overwrite"
